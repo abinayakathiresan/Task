e14da1a62a99bae1bc6727b8e10341c5bf343fa9
abinayakathiresan <abinayakathiresan7@gmail.com>
2024-05-16 16:33:00 +0530

    task

--- src/Components/ManageTask/ManageTask.jsx
+++ src/Components/ManageTask/ManageTask.jsx
@@ -1,8 +1,13 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const ManageTask = ()=> {
 return(
-  <div>
+  <div>dsf
+    <Box>
+    <Typography variant="h1" component="h2">Task List</Typography>
+    <Button variant="contained">Add Task</Button>
+    </Box>
   </div>
 )
 
